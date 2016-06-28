fa3b7c555813e05df9dc998bfb7a45e8de5fb6a9
jluong <jason.qh.luong@gmail.com>
2016-06-28 01:39:19 +0000

    Added extra test cases to rspec, refactored fizzbuzz.rb

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,20 @@
+#require 'regexp'
+
 def fizzbuzz(number)
-    'fizz'
+    begin
+        Integer(number)
+    rescue
+        puts "This method only accepts integers as aguments, \"#{number}\" is not an integer."
+        exit
+    end
+    
+    if (number % 3 == 0) && (number % 5 == 0)
+        "fizzbuzz"
+    elsif number % 3 == 0
+        "fizz"
+    elsif number % 5 == 0
+        "buzz"
+    else
+        number
+    end
 end
